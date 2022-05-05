9ee02241fbae1e92b71775099923cd4be5bee14c
DennisPerez97 <dennis.perez.97@hotmail.com>
2022-05-04 19:16:52 -0500

    Agregando Express como servidor de la API, se añadio los diferentes endpoints requeridos en la practica

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -8,10 +8,26 @@ describe("Test para Controller: getStudent",() =>{
     });
 });
 
-describe("Test para Controller: getStudent",() =>{
-    test("Requerimiento 1: Calcular los estudiantes",() => {
+describe("Test para Controller: getStudentsByCertification",() =>{
+    test("Requerimiento 2: Calcular los estudiantes con certificacion",() => {
         const students = StudentController.getStudentsByCertification();
         expect(students.length).toBe(29);
+
+    });
+});
+
+describe("Test para Controller: getEmailStudentsByCertification",() =>{
+    test("Requerimiento 3: Calcular el email de los estudiantes con certificado",() => {
+        const students = StudentController.getEmailStudentsByCertification();
+        expect(students.length).toBe(29);
+
+    });
+});
+
+describe("Test para Controller: getStudentByCredits",() =>{
+    test("Requerimiento 4: Calcular los estudiantes por creditos mayores a 500",() => {
+        const students = StudentController.getStudentByCredits();
+        expect(students.length).toBe(27);
 
     });
 });
